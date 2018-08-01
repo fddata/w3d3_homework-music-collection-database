60e7c7c648b5f44363505a22314f5802155c0d75
fddata <fraserjdouglas@gmail.com>
2018-08-01 14:37:17 +0100

    adds albums, artists and SqlRunner classes.

--- db/music_library.sql
+++ db/music_library.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS ablums;
 DROP TABLE IF EXISTS artists;
 
-CREATE TABLE albums(
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255),
-  genre VARCHAR(255)
-);
-
 CREATE TABLE artists(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255)
+);
 
+CREATE TABLE albums(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  genre VARCHAR(255),
+  artist_id INT REFERENCES artists(id)
 );
---
+++ models/albums.rb
@@ -0,0 +1,18 @@
+require 'pg'
+
+
+class Album
+
+  attr_reader :id, :artist_id
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id']
+  end
+
+
+
+
+end
