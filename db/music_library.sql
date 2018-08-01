1f1779dff25cd97832f9b430317eec4cfc96c9d6
fddata <fraserjdouglas@gmail.com>
2018-08-01 14:21:32 +0100

    initial commit, establishes file structure

---
+++ db/music_library.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS ablums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE albums(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  genre VARCHAR(255)
+);
+
+CREATE TABLE artists(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+
+);
